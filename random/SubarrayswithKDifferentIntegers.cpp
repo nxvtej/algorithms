c956803467ca97aa28534ee886f6618aaad6f2a1
nxvtej <nxvtej@outlook.com>
2024-08-20 19:54:30 +0530

    sliding window

---
+++ random/SubarrayswithKDifferentIntegers.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int slidingWindow(vector<int> &nums, int k)
+{
+    unordered_map<int, int> map;
+    int left = 0, right = 0, ans = 0;
+    while (right < nums.size())
+    {
+        map[nums[right]]++;
+        while (map.size() > k)
+        {
+            map[nums[left]]--;
+            if (map[nums[left]] == 0)
+                map.erase(nums[left]);
+            left++;
+        }
+        ans += right - left + 1; // basically the size of subarray;
+        right++;
+    }
+    return ans;
+}
+int findSubarraysWithKdifferentIntegers(vector<int> &arr, int n, int k)
+{
+    int forKdifferentIntegers = slidingWindow(arr, k);
+    int forLessThanKdifferentIntegers = slidingWindow(arr, k - 1);
+
+    return forKdifferentIntegers - forLessThanKdifferentIntegers;
+}
+int main()
+{
+    vector<int> arr = {50, 52, 65, 9, 55, 55, 6, 1, 1, 2, 2, 0};
+    int n = arr.size();
+
+    int k = 2;
+    int count = findSubarraysWithKdifferentIntegers(arr, n, k);
+    cout << "\n count " << count;
+    return 0;
+}
--- worksheet/demo.cpp
+++ worksheet/demo.cpp
@@ -1,19 +1,35 @@
-class Solution {
+#include <iostream>
+
+class Base
+{
 public:
-    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
-        int max_index=0, max_diagonal=0,i=0;
-        
-        for(auto it: dimensions){
-            if(sqrt((it[0]*it[0])+(it[1]*it[1]))>=max_diagonal){
-                max_diagonal = sqrt((it[0]*it[0])+(it[1]*it[1]));
-                max_index = i;
-            }
-            i++;
-        }
-        
-//         step2: calculate area of rectangle 
-        int area=0;
-        auto it=dimensions[max_index];
-        return it[0]*it[1];        
+    void print()
+    { // Non-virtual function
+        std::cout << "Base class print function" << std::endl;
     }
 };
+
+class Derived : public Base
+{
+public:
+    void print()
+    { // Non-virtual function
+        std::cout << "Derived class print function" << std::endl;
+    }
+};
+
+class CC : public Derived
+{
+public:
+    void print()
+    { // Non-virtual function
+        std::cout << "Derived 2 class print function" << std::endl;
+    }
+};
+
+int main()
+{
+    Derived *a = new CC();
+    a->print();
+    return 0;
+}
